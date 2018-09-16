a2ceab3731cc84501f41f37e1c88a729451e9e6a
snowkylin <xihanli316@gmail.com>
2018-09-16 16:34:08 +0800

    update

--- _static/code/zh/model/rnn/rnn.py
+++ _static/code/zh/model/rnn/rnn.py
@@ -3,12 +3,6 @@
 import tensorflow as tf
 import numpy as np
 
-tf.enable_eager_execution()
-num_batches = 10000
-seq_length = 40
-batch_size = 50
-learning_rate = 1e-3
-
 
 class RNN(tf.keras.Model):
     def __init__(self, num_chars):
@@ -55,23 +49,30 @@ class DataLoader():
         return np.array(seq), np.array(next_char)       # [num_batch, seq_length], [num_batch]
 
 
-data_loader = DataLoader()
-model = RNN(len(data_loader.chars))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-for batch_index in range(num_batches):
-    X, y = data_loader.get_batch(seq_length, batch_size)
-    with tf.GradientTape() as tape:
-        y_logit_pred = model(X)
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_logit_pred)
-        print("batch %d: loss %f" % (batch_index, loss.numpy()))
-    grads = tape.gradient(loss, model.variables)
-    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+if __name__ == '__main__':
+    tf.enable_eager_execution()
+    num_batches = 10000
+    seq_length = 40
+    batch_size = 50
+    learning_rate = 1e-3
 
-X_, _ = data_loader.get_batch(seq_length, 1)
-for diversity in [0.2, 0.5, 1.0, 1.2]:
-    X = X_
-    print("diversity %f:" % diversity)
-    for t in range(400):
-        y_pred = model.predict(X, diversity)
-        print(data_loader.indices_char[y_pred[0]], end='', flush=True)
-        X = np.concatenate([X[:, 1:], np.expand_dims(y_pred, axis=1)], axis=-1)
+    data_loader = DataLoader()
+    model = RNN(len(data_loader.chars))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    for batch_index in range(num_batches):
+        X, y = data_loader.get_batch(seq_length, batch_size)
+        with tf.GradientTape() as tape:
+            y_logit_pred = model(X)
+            loss = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_logit_pred)
+            print("batch %d: loss %f" % (batch_index, loss.numpy()))
+        grads = tape.gradient(loss, model.variables)
+        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+
+    X_, _ = data_loader.get_batch(seq_length, 1)
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        X = X_
+        print("diversity %f:" % diversity)
+        for t in range(400):
+            y_pred = model.predict(X, diversity)
+            print(data_loader.indices_char[y_pred[0]], end='', flush=True)
+            X = np.concatenate([X[:, 1:], np.expand_dims(y_pred, axis=1)], axis=-1)
