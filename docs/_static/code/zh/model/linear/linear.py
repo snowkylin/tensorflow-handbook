91eb5daef4857907c216ca5d5aeb82ca2ce94b4a
snowkylin <xihanli316@gmail.com>
2019-03-03 16:56:46 +0800

    update compiled docs

--- docs/_static/code/zh/model/linear/linear.py
+++ docs/_static/code/zh/model/linear/linear.py
@@ -8,8 +8,12 @@ y = tf.constant([[10.0], [20.0]])
 class Linear(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        self.dense = tf.keras.layers.Dense(units=1, kernel_initializer=tf.zeros_initializer(),
-            bias_initializer=tf.zeros_initializer())
+        self.dense = tf.keras.layers.Dense(
+            units=1,
+            activation=None,
+            kernel_initializer=tf.zeros_initializer(),
+            bias_initializer=tf.zeros_initializer()
+        )
 
     def call(self, input):
         output = self.dense(input)
@@ -21,8 +25,8 @@ model = Linear()
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 for i in range(100):
     with tf.GradientTape() as tape:
-        y_pred = model(X)      # 调用模型
+        y_pred = model(X)      # 调用模型 y_pred = model(X) 而不是显式写出 y_pred = a * X + b
         loss = tf.reduce_mean(tf.square(y_pred - y))
-    grads = tape.gradient(loss, model.variables)
+    grads = tape.gradient(loss, model.variables)    # 使用 model.variables 这一属性直接获得模型中的所有变量
     optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
 print(model.variables)
