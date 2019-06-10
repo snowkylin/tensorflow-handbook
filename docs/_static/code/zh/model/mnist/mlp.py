d61261890e065c0c6dc9ae8416fdee5b3a2f913b
snowkylin <xihanli316@gmail.com>
2019-06-11 03:40:28 +0800

    update online docs

---
+++ docs/_static/code/zh/model/mnist/mlp.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+class MLP(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.flatten = tf.keras.layers.Flatten()    # Flatten层将除第一维（batch_size）以外的维度展平
+        self.dense1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(units=10)
+
+    def call(self, inputs):         # [batch_size, 28, 28, 1]
+        x = self.flatten(inputs)    # [batch_size, 784]
+        x = self.dense1(x)          # [batch_size, 100]
+        x = self.dense2(x)          # [batch_size, 10]
+        output = tf.nn.softmax(x)
+        return output
