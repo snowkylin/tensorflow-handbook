a01c739d0c7c2aaf10c8f529046329eab769f341
snowkylin <xihanli316@gmail.com>
2019-06-18 22:53:35 +0800

    update online docs

--- docs/_static/code/zh/basic/eager/1plus1.py
+++ docs/_static/code/zh/basic/eager/1plus1.py
@@ -1,13 +1,23 @@
 import tensorflow as tf
 
-a = tf.constant(1)
-b = tf.constant(1)
-c = tf.add(a, b)    # 也可以直接写 c = a + b，两者等价
+# 定义一个随机数（标量）
+random_float = tf.random.uniform(shape=())
 
-print(c)
+# 定义一个有2个元素的零向量
+zero_vector = tf.zeros(shape=(2))
 
-A = tf.constant([[1, 2], [3, 4]])
-B = tf.constant([[5, 6], [7, 8]])
-C = tf.matmul(A, B)
+# 定义两个2×2的常量矩阵
+A = tf.constant([[1., 2.], [3., 4.]])
+B = tf.constant([[5., 6.], [7., 8.]])
+
+# 查看矩阵A的形状、类型和值
+print(A.shape)      # 输出(2, 2)，即矩阵的长和宽均为2
+print(A.dtype)      # 输出<dtype: 'float32'>
+print(A.numpy())    # 输出[[1. 2.]
+                    #      [3. 4.]]
+
+C = tf.add(A, B)    # 计算矩阵A和B的和
+D = tf.matmul(A, B) # 计算矩阵A和B的乘积
 
 print(C)
+print(D)
