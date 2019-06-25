59739aa7d7f2c00fcb3812700c614bcc7d334d5f
snowkylin <xihanli316@gmail.com>
2019-06-25 23:15:25 +0800

    update CheckpointManager in tf 2.0 and TensorBoard

--- source/_static/code/zh/tools/tensorboard/mnist.py
+++ source/_static/code/zh/tools/tensorboard/mnist.py
@@ -1,22 +1,22 @@
 import tensorflow as tf
 from zh.model.mnist.mlp import MLP
-from zh.model.utils import DataLoader
+from zh.model.utils import MNISTLoader
 
-tf.enable_eager_execution()
 num_batches = 10000
 batch_size = 50
 learning_rate = 0.001
 model = MLP()
-data_loader = DataLoader()
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-summary_writer = tf.contrib.summary.create_file_writer('./tensorboard')     # 实例化记录器
-with summary_writer.as_default(), tf.contrib.summary.always_record_summaries():
-    for batch_index in range(num_batches):
-        X, y = data_loader.get_batch(batch_size)
-        with tf.GradientTape() as tape:
-            y_logit_pred = model(tf.convert_to_tensor(X))
-            loss = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_logit_pred)
-            print("batch %d: loss %f" % (batch_index, loss.numpy()))
-            tf.contrib.summary.scalar("loss", loss, step=batch_index)       # 记录当前loss
-        grads = tape.gradient(loss, model.variables)
-        optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+data_loader = MNISTLoader()
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+summary_writer = tf.summary.create_file_writer('./tensorboard')     # 实例化记录器
+for batch_index in range(num_batches):
+    X, y = data_loader.get_batch(batch_size)
+    with tf.GradientTape() as tape:
+        y_pred = model(X)
+        loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+        loss = tf.reduce_mean(loss)
+        print("batch %d: loss %f" % (batch_index, loss.numpy()))
+        with summary_writer.as_default():                           # 指定记录器
+            tf.summary.scalar("loss", loss, step=batch_index)       # 将当前损失函数的值写入记录器
+    grads = tape.gradient(loss, model.variables)
+    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
