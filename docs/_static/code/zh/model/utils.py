c9e5d987ac0d9bd3854a4e4c4c018d7bc969f115
snowkylin <xihanli316@gmail.com>
2019-07-23 23:19:40 +0800

    update online docs

--- docs/_static/code/zh/model/utils.py
+++ docs/_static/code/zh/model/utils.py
@@ -6,6 +6,7 @@ class MNISTLoader():
     def __init__(self):
         mnist = tf.keras.datasets.mnist
         (self.train_data, self.train_label), (self.test_data, self.test_label) = mnist.load_data()
+        # MNIST中的图像默认为uint8（0-255的数字）。以下代码将其归一化到0-1之间的浮点数，并在最后增加一维作为颜色通道
         self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)      # [60000, 28, 28, 1]
         self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)        # [10000, 28, 28, 1]
         self.train_label = self.train_label.astype(np.int32)    # [60000]
@@ -13,5 +14,6 @@ class MNISTLoader():
         self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
 
     def get_batch(self, batch_size):
+        # 从数据集中随机取出batch_size个元素并返回
         index = np.random.randint(0, np.shape(self.train_data)[0], batch_size)
         return self.train_data[index, :], self.train_label[index]
