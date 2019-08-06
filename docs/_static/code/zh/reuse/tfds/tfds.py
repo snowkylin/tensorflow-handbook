3231838a6563d4b021ed7a26d3d0a73955b3d90f
snowkylin <xihanli316@gmail.com>
2019-08-06 09:52:42 +0800

    update online docs

--- docs/_static/code/zh/reuse/tfds/tfds.py
+++
@@ -1,9 +0,0 @@
-import tensorflow_datasets as tfds
-
-beam = tfds.core.lazy_imports.apache_beam
-
-dl_config = tfds.download.DownloadConfig(
-    beam_options=beam.options.pipeline_options.PipelineOptions()
-)
-wp = tfds.load("wikipedia")
-wp.download_and_prepare(download_config=dl_config)
