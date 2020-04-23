f32eb657f1c381e15543795c796e0d3f93c0ef2f
snowkylin <xihanli316@gmail.com>
2020-04-23 17:29:22 +0800

    remove .numpy()

--- source/_static/code/zh/basic/eager/grad.py
+++ source/_static/code/zh/basic/eager/grad.py
@@ -4,7 +4,7 @@ x = tf.Variable(initial_value=3.)
 with tf.GradientTape() as tape:     # 在 tf.GradientTape() 的上下文内，所有计算步骤都会被记录以用于求导
     y = tf.square(x)
 y_grad = tape.gradient(y, x)        # 计算y关于x的导数
-print([y, y_grad])
+print(y, y_grad)
 
 X = tf.constant([[1., 2.], [3., 4.]])
 y = tf.constant([[1.], [2.]])
@@ -13,4 +13,4 @@ b = tf.Variable(initial_value=1.)
 with tf.GradientTape() as tape:
     L = tf.reduce_sum(tf.square(tf.matmul(X, w) + b - y))
 w_grad, b_grad = tape.gradient(L, [w, b])        # 计算L(w, b)关于w, b的偏导数
-print([L.numpy(), w_grad.numpy(), b_grad.numpy()])
+print(L, w_grad, b_grad)
